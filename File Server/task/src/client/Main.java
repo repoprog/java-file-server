a603be8ef255ccd538a2dd3fa8dc2637247d88d6
Remigiusz <repogplus@gmail.com>
2022-02-03 21:05:55 +0100

    stage completed

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -5,30 +5,103 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client();
+        System.out.println("System.out.println(Enter action (1 - get a file, 2 - create a file, 3 - delete a file):);\n");
+
+        while (true) {
+            String action = scanner.nextLine();
+            switch (action) {
+                case "1" -> client.getFile();
+                case "2" -> client.createFile();
+                case "3" -> client.deleteFile();
+//                case "exit" ->
+                default -> System.out.println("Wrong command");
+            }
+        }
+    }
+
+}
+
+class Client {
     private static final String address = "127.0.0.1";
     private static final int port = 23456;
+    Scanner scanner = new Scanner(System.in);
+    String PUT = "put";
+    String GET = "get";
+    String DELETE = "delete";
 
-    public static void main(String[] args) {
 
-        System.out.println("Client started!");
+    public void getFile() {
+        System.out.println("Enter filename: ");
+        getRequest(scanner.nextLine());
+    }
 
-        try (Socket socket = new Socket(InetAddress.getByName(address), port);
-             DataInputStream input = new DataInputStream(socket.getInputStream());
-             DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+    public void createFile() {
+        System.out.println("Enter filename: ");
+        String fileName = scanner.nextLine();
+        System.out.println("Enter file content: ");
+        String content = scanner.nextLine();
+        putRequest(fileName, content);
+    }
+
+    public void deleteFile() {
+        System.out.println("Enter filename: ");
+        deleteRequest(scanner.nextLine());
+    }
+
+
+    public void getRequest(String fileName) {
+
+        try (
+                Socket socket = new Socket(InetAddress.getByName(address), port);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream())
         ) {
-            String sent = "Give me everything you have!";
+
+            String sent = GET + " " + fileName;
             output.writeUTF(sent);
             String received = input.readUTF();
-
+            System.out.println("The request was sent.");
             System.out.println("Sent: " + sent +
                     "\nReceived: " + received);
 
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
     }
+
+    public void putRequest(String fileName, String content) {
+
+        try (
+                Socket socket = new Socket(InetAddress.getByName(address), port);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+        ) {
+
+            String sent = PUT + " " + fileName;
+            output.writeUTF(sent);
+            String received = input.readUTF();
+            System.out.println("The request was sent.");
+            System.out.println("Sent: " + sent +
+                    "\nReceived: " + received);
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteRequest(String file) {
+
+        System.out.println("The request was sent.");
+    }
+
 }
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -6,7 +6,24 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
+enum ResponseStatus {
+    OK(200),
+    FORBIDDEN(403),
+    NOT_FOUND(404);
+
+    int code;
+
+    ResponseStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
 
 public class Main {
 
@@ -14,6 +31,7 @@ public class Main {
     private static final int port = 23456;
 
     public static void main(String[] args) {
+
         System.out.println("Server started!");
 
         try (ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address))) {
@@ -24,16 +42,69 @@ public class Main {
                      DataOutputStream output = new DataOutputStream(socket.getOutputStream())
                 ) {
                     String received = input.readUTF();
-                    String sent = "All files were sent!";
+                    String sent = processRequest(received);
                     System.out.println("Received: " + received +
                             "\nSent: " + sent);
                     output.writeUTF(sent);
 
-                    quit = true; // end connection after one receive/sent
+//                    quit = true; // end connection after one receive/sent
                 }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    static FileServer fileServer = new FileServer();
+
+    public static String processRequest(String received) {
+        String[] rcvParts = received.split(" ");
+        String reqType = rcvParts[0];
+        String fileName = rcvParts[1];
+        String fileContent = rcvParts.length == 3 ? rcvParts[2] : "";
+     return  switch (reqType) {
+            case "get" -> fileServer.getFile(fileName);
+            case "put" -> fileServer.addFile(fileName, fileContent);
+//            case "delete" -> fileServer.deleteFile(fileName);
+           default -> "bad request";
+        };
+    }
 }
+
+class FileServer {
+    private List<String> filesList;
+
+    public FileServer() {
+        this.filesList = new ArrayList<>();
+    }
+
+    public String getFile(String fileName) {
+        if (filesList.contains(fileName)) {
+            filesList.indexOf(fileName);
+           return ResponseStatus.OK.getCode() + " " + fileName;
+        } else {
+            return ResponseStatus.NOT_FOUND.getCode() + " " + fileName;
+        }
+    }
+
+    public String addFile(String fileName, String fileContent) {
+//        String regexp = "file([1-9]|10)"; fileName.matches(regexp) &&
+        if (!filesList.contains(fileName)) {
+            filesList.add(fileName);
+            return ResponseStatus.OK.getCode() + " " + fileName;
+        } else {
+          return   ResponseStatus.NOT_FOUND.getCode() + " " + fileName;
+        }
+    }
+
+
+    public void deleteFile(String fileName) {
+        if (filesList.contains(fileName)) {
+            filesList.remove(fileName);
+            System.out.println("The file " + fileName + " was deleted");
+        } else {
+            System.out.println("The file " + fileName + " not found");
+        }
+    }
+}
+
