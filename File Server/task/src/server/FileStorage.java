5aa04090a91a598111e626c28dd866de3b1c28de
Remigiusz <repogplus@gmail.com>
2022-03-21 22:48:19 +0100

    stage completed

---
+++ File Server/task/src/server/FileStorage.java
@@ -0,0 +1,91 @@
+package server;
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FileStorage {
+
+    private static int fileId = 0;
+    private static Map<Integer, String> filesIndex = new HashMap<>();
+    private static final String ROOT = System.getProperty("user.dir") +
+            File.separator + "src" +
+            File.separator + "server" +
+            File.separator + "data" +
+            File.separator;
+
+    static final String INDEX_PATH = System.getProperty("user.dir") +
+            File.separator + "src" +
+            File.separator + "storage" +
+            File.separator + "data" +
+            File.separator;
+    static String FILE = "indexes.data";
+
+    public static void makeDirs() {
+        File folder = new File(ROOT);
+        folder.mkdirs();
+    }
+
+    public static void setFileId(int fileId) {
+        FileStorage.fileId = fileId;
+    }
+
+    public static void setFilesIndex(Map<Integer, String> filesIndex) {
+        FileStorage.filesIndex = filesIndex;
+    }
+
+    public static String getFile(Session session, String getterType, String requiredFile) {
+        if (getterType.equals("BY_ID")) {
+            requiredFile = getFileNameById(Integer.parseInt(requiredFile));
+        }
+        try {
+           session.setSavedContent((Files.readAllBytes(Paths.get(ROOT + requiredFile))));
+            return ResponseStatus.OK.getCode() + " ";
+        } catch (IOException e) {
+            return ResponseStatus.NOT_FOUND.getCode() + "";
+        }
+    }
+
+    public static String saveFile(String fileName, byte[] fileContent) {
+        File file = new File(ROOT + fileName);
+        if (!file.exists()) {
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                fos.write(fileContent);
+                ++fileId;
+                filesIndex.put(fileId, fileName);
+                SerializationUtils.serialize(filesIndex, INDEX_PATH, FILE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return ResponseStatus.OK.getCode() + " " + fileId;
+        } else {
+            return ResponseStatus.FORBIDDEN.getCode() + "";
+        }
+    }
+
+    public static String deleteFile(String getterType, String requiredFile) {
+        if (getterType.equals("BY_ID")) {
+            int id = Integer.parseInt(requiredFile);
+            requiredFile = getFileNameById(id);
+            filesIndex.remove(id);
+        }
+
+        File file = new File(ROOT + requiredFile);
+        filesIndex.values().remove(requiredFile);
+        return file.delete() ? ResponseStatus.OK.getCode() + ""
+                : ResponseStatus.NOT_FOUND.getCode() + "";
+    }
+
+    public static String getFileNameById(int fileId) {
+        return filesIndex.getOrDefault(fileId, "no such file");
+    }
+
+    public static String showFiles() {
+        System.out.println(filesIndex);
+        return "";
+    }
+}
